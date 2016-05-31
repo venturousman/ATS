c9f3354dfe7bfad2fea7f2040a61ecfc1b2628ef
venturousman <venturousman@yahoo.com>
2016-05-31 17:40:16 +0700

    [FrontOffice] add main.js

--- ATS.FrontOffice/app/app.js
+++ ATS.FrontOffice/app/app.js
@@ -1,1 +1,18 @@
-﻿
+﻿/*global require*/
+'use strict';
+
+require([
+	'angular'
+], function (angular) {
+    //require([
+	//	'controllers/todo',
+	//	'directives/todoFocus',
+	//	'directives/todoEscape',
+	//	'services/todoStorage'
+    //], function (todoCtrl, todoFocusDir, todoEscapeDir, todoStorageSrv) {
+    //    angular
+	//		.module('ats', [todoFocusDir, todoEscapeDir, todoStorageSrv])
+	//		.controller('TodoController', todoCtrl);
+    //    angular.bootstrap(document, ['ats']);
+    //});
+});
---
+++ ATS.FrontOffice/app/main.js
@@ -0,0 +1,19 @@
+﻿/*global require*/
+'use strict';
+
+require.config({
+    //baseUrl: 'app',
+    paths: {
+        // Aliases and paths of modules
+        angular: '../scripts/angular',
+        'angular-route': '../scripts/angular-route',
+    },
+    shim: {
+        // Modules and their dependent modules
+        angular: {
+            exports: 'angular'
+        },
+        'angular-route': ['angular']
+    },
+    deps: ['app']
+});
